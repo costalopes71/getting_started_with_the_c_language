1929c366c5a977d68177808ae60f2f6440328263
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-09-14 23:21:15 -0300

    hello world

---
+++ hello_world/hello_world.c
@@ -0,0 +1,13 @@
+/* this is a C comment 
+   it is multiline
+*/
+
+// this is a line comment (C++ comment)
+
+#include <stdio.h> 
+
+int main() {
+    printf("Hello World! =D\n");
+    
+    return 0;
+}
